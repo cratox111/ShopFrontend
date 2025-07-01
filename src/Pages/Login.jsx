0d5836c6f38a58872873b01f1eef18dfb93c8dbb
cratox111 <diocgon2011@gmail.com>
2025-06-30 21:48:04 -0600

    Añadi el login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,4 +1,6 @@
 import MenuMain from "../Items/MenuMain";
+import '../assets/CSS/index.css'
+import '../assets/CSS/Login/styleLogin.css'
 
 const Login = () => {
     return (
@@ -6,7 +8,28 @@ const Login = () => {
             <MenuMain />
             <div className="container-main">
                 <div className="container-login">
-                    xvfd
+                    <h1>Login</h1>
+                    <form action="">
+                        <div className="container-input">
+                            <label htmlFor="" className="text-label">Name:</label>
+                            <input type="text" className="input-text" />
+                        </div>
+                        <div className="container-input">
+                            <label htmlFor="" className="text-label">Email:</label>
+                            <input type="text" className="input-text" />
+                        </div>
+                        <div className="container-input">
+                            <label htmlFor="" className="text-label">Password:</label>
+                            <input type="text" className="input-text" />
+                        </div>
+                        <div className="container-terms">
+                            <input type="checkbox" />
+                            <p>Aceptas terminos y condiciones</p>
+                        </div>
+                        <div>
+                            <button className="btn-main">Login</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </>
